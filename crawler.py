8f7a3a54d48b5330ddc818c3a28a72c1f604a6a3
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-03 14:28:51 -0400

    removed database directory and added django project

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@ url_list = ["https://futurereadypa.org/Home/DataFiles"]
 for url in url_list:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    data_blocks = soup.find_all('a')
+    data_blocks = soup.find_all("td")
     for data in data_blocks:
         print(data)
     
