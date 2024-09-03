9f7bbbca0874ae92ef254895f40bd63644949ede
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-03 13:51:32 -0400

    fixed a crawler bug

--- crawler.py
+++ crawler.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url_list = []
+url_list = ["https://futurereadypa.org/Home/DataFiles"]
 
 for url in url_list:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     data_blocks = soup.find_all('a')
+    for data in data_blocks:
+        print(data)
     
