8f391d292b4c9883f58018793fb6e2fa26622378
Alexander <dearinja@lafayette.edu>
2024-09-03 14:31:48 -0400

    scapper scapper

--- crawler.py
+++ crawler.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url_list = ["https://futurereadypa.org/Home/DataFiles"]
+url_list = ["https://futurereadypa.org/Home/DataFiles", ""]
 
 for url in url_list:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    data_blocks = soup.find_all("td")
-    for data in data_blocks:
-        print(data)
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        data_blocks = soup.find_all(lambda tag: tag.name == 'a' and tag.get('href') and tag['href'].startswith('/home/getdatafile?'))
+        for data in data_blocks:
+            print(data)
+    except requests.exceptions.MissingSchema:
+        print(f"Invalid URL: {url}")
     
