e164cc6899609c75def93b8cfc91846a9f81cfab
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-03 13:45:37 -0400

    added more to the crawler

--- crawler.py
+++ crawler.py
@@ -1,3 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+url_list = []
+
+for url in url_list:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    data_blocks = soup.find_all('a')
+    
