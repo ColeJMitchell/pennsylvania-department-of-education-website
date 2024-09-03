0dbc5c9c30a43a3b11b2f7fde76517833e5692cd
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-03 17:58:09 -0400

    added scripts and makefile so that we can add tables to the django database using a text editor

---
+++ src/sql/build.sql
@@ -0,0 +1,13 @@
+create table test(
+	id int primary key,
+	year int not null,
+	district_name text not null,
+	aun int not null,
+	data_element text not null,
+	display_value text not null
+);
+
+
+
+
+.separator ","
