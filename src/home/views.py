bbea31f2889ada32621801630b16f97b919b07c6
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-05 14:01:35 -0400

    set the template path based on developers operating system and created a template directory to store html files that will be rendered based  on the url routing

--- src/home/views.py
+++ src/home/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_page(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+def home_page(request, *args, **kwargs):
+    return render(request, "home.html", {})
 
-def login_page(*args, **kwargs):
-    return HttpResponse("<h1>This is a login page</h1>")
+def login_page(request, *args, **kwargs):
+    return render(request, "login.html", {})
