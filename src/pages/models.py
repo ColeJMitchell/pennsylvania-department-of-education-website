23811b93e6d3d25b7a02a6714fcf13f9c5369ea5
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-10 15:04:13 -0400

    attempt at displaying models in the html

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.db import models
 
 class entries(models.Model):
     year = models.IntegerField()
     aun = models.IntegerField()
     county = models.TextField(max_length=1000)
-    salary = models.IntegerField()
     name = models.TextField(max_length=1000)
 
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import entries
 
 # Create your views here.
 def home_page(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    data = entries.objects.all()
+    context = {entries:data}
+    return render(request, "home.html", context)
