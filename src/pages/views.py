609c96aa777d7c2a3906c5912ce3f11b649c2e8a
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-10 20:06:11 -0400

    frontend now display columns stored in database

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import entries
 
 # Create your views here.
-def home_page(request, *args, **kwargs):
+def home_page(request):
     data = entries.objects.all()
-    context = {entries:data}
+    context = {'entries':data}
     return render(request, "home.html", context)
