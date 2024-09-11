a2892ec76212f1b9ae1c10b7adbd337d3cde75e4
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-10 21:31:58 -0400

    when user submits post request through html button the first column in the years column is incremented by one

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,6 +3,16 @@ from .models import entries
 
 # Create your views here.
 def home_page(request):
+    #Once a user pressed update a post request is made
+    if request.method == 'POST':
+        data = entries.objects.all()
+        context = {'entries':data}
+        first_entry = entries.objects.first() 
+        first_entry.year += 1 
+        first_entry.save()
+
+
+    #First get request when user first enters our website
     data = entries.objects.all()
     context = {'entries':data}
     return render(request, "home.html", context)
