b4fe876fa01f5a12871d560c8eb043f2fd7c709f
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-10 22:15:23 -0400

    you can now add rows to the database from the frontend by clicking a button

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,11 +5,13 @@ from .models import entries
 def home_page(request):
     #Once a user pressed update a post request is made
     if request.method == 'POST':
-        data = entries.objects.all()
-        context = {'entries':data}
-        first_entry = entries.objects.first() 
-        first_entry.year += 1 
-        first_entry.save()
+        if 'update' in request.POST:
+            first_entry = entries.objects.first() 
+            first_entry.year += 1 
+            first_entry.save()
+        elif 'write' in request.POST:
+            new_entry = entries(year = 2017, aun = 112358132, county = "Washington", name = "Cheston")
+            new_entry.save()
 
 
     #First get request when user first enters our website
