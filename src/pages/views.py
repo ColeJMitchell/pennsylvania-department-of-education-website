b34a37c9ff6a083da1d60ab4878b97b86165944d
Cole Mitchell <colejmitchell.cs@gmail.com>
2024-09-11 10:58:41 -0400

    Update views.py

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,7 +3,7 @@ from .models import entries
 
 # Create your views here.
 def home_page(request):
-    #Once a user pressed update a post request is made
+    #Once a user pressed update or write a post request is made
     if request.method == 'POST':
         if 'update' in request.POST:
             first_entry = entries.objects.first() 
