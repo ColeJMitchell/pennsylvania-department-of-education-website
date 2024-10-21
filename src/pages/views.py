70e7d03c1dab3c3592663c935a71efa56035d162
geladzel <geladzel@lafayette.edu>
2024-10-20 20:27:12 -0400

    fixing comparison

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from .models import enrollment
+import itertools
 
 # Create your views here.
 def home_page(request):
@@ -46,7 +47,6 @@ def plot_page(request):
     else:
         return render(request, "plot.html", {})
 
-
 def comparison_page(request):
     if request.method == "POST":
         # Retrieve values for the first set of inputs
@@ -62,34 +62,36 @@ def comparison_page(request):
         # Filters for the first set of inputs
         filters1 = {}
 
-        if school_submit != "Select Schools":
+        if school_submit != "Select School":
             filters1['school'] = school_submit
-        if district_submit != "Select Districts":
+        if district_submit != "Select District":
             filters1['district'] = district_submit
         if year_submit != "Select Years":
             filters1['years'] = year_submit
 
-        # filters for the second set of inputs
+        # Filters for the second set of inputs
         filters2 = {}
 
-        if school_submit2 != "Select Schools 2":
+        if school_submit2 != "Select School 2":
             filters2['school'] = school_submit2
-        if district_submit2 != "Select Districts 2":
+        if district_submit2 != "Select District 2":
             filters2['district'] = district_submit2
         if year_submit2 != "Select Years 2":
             filters2['years'] = year_submit2
 
-        # Query db for data
-        data1 = enrollment.objects.filter(**filters1) if filters1 else None
-        data2 = enrollment.objects.filter(**filters2) if filters2 else None
+        # Query db for data or set to empty list if filters are empty
+        data1 = enrollment.objects.filter(**filters1) if filters1 else []
+        data2 = enrollment.objects.filter(**filters2) if filters2 else []
+
+        # Combine data1 and data2 using zip_longest
+        combined_enrollment = itertools.zip_longest(data1, data2, fillvalue=None)
 
         # Create context
         context = {
-            'enrollment1': data1,
-            'school_submit1': school_submit,
-            'district_submit1': district_submit,
-            'year_submit1': year_submit,
-            'enrollment2': data2,
+            'combined_enrollment': combined_enrollment,
+            'school_submit': school_submit,
+            'district_submit': district_submit,
+            'year_submit': year_submit,
             'school_submit2': school_submit2,
             'district_submit2': district_submit2,
             'year_submit2': year_submit2,
