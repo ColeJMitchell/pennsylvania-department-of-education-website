7fb3a8cedab6ed17236f6ecb52c80729e9e91967
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-12-09 18:26:00 -0500

    redid some of the plot page

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from .models import enrollment
+from django.http import JsonResponse
 import itertools
 
 # Create your views here.
@@ -14,36 +15,13 @@ def map_page(request):
     api_key = settings.MAP_API_KEY
     if request.method == "POST":
         return render(request, "map.html", {"api_key": api_key})
-    
+
+
 def plot_page(request):
     if request.method == "POST":
-        school_submit = request.POST.get('school_submit')
-        district_submit = request.POST.get('district_submit')
-        year_submit = request.POST.get('year_submit')
-
-        #Filters that were selected in the front end
-        filters = {}
-        if school_submit != "Select Schools":
-            filters['school'] = school_submit
-        if district_submit != "Select Districts":
-            filters['district'] = district_submit
-        if year_submit != "Select Years":
-            filters['years'] = year_submit
-
-        if filters:
-            data = enrollment.objects.filter(**filters)
-        else:
-            data = None 
-        # Create context
-        context = {
-            'enrollment': data,
-            'school_submit': school_submit,
-            'district_submit': district_submit,
-            'year_submit': year_submit,
-        }
-        print("Filters:", filters)
-        print("Data:", data)
-        return render(request, "plot.html", context)
+        school_submit = request.POST.get('query')
+        print(school_submit)
+        return render(request, "plot.html", {"school_submit": school_submit})
     else:
         return render(request, "plot.html", {})
 
