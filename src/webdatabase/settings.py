473f50b672e0de592ef10dd04f9c9ed1939842cb
geladzel <geladzel@lafayette.edu>
2024-12-10 16:20:09 -0500

    map caching implemented

--- src/webdatabase/settings.py
+++ src/webdatabase/settings.py
@@ -104,6 +104,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
