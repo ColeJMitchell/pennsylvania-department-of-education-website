5577baee6e6d254e5ee095d6c616d97abd89b25d
geladzel <geladzel@lafayette.edu>
2024-12-11 19:05:57 -0500

    district keystone uploaded

--- src/delete_all.py
+++ src/delete_all.py
@@ -4,9 +4,9 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webdatabase.settings')
 django.setup()
 
-from pages.models import district
+from pages.models import districtFiscal
 
 # Delete all records in the district table
-district.objects.all().delete()
+districtFiscal.objects.all().delete()
 
 print("All records in the district table have been deleted.")
