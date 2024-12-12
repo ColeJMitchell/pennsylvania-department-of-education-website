8650a264880c4dc4e3a3101d7e5575b59c90fd4b
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-12-11 19:24:23 -0500

    working on comparison page

--- src/pages/views.py
+++ src/pages/views.py
@@ -87,55 +87,10 @@ def plot_page(request):
     else:
         return render(request, "plot.html", {})
 
+#code to compare two schools in the front-end based on context sent from the front end
 def comparison_page(request):
     if request.method == "POST":
-        # Retrieve values for the first set of inputs
-        school_submit = request.POST.get('school_submit')
-        district_submit = request.POST.get('district_submit')
-        year_submit = request.POST.get('year_submit')
-
-        # Retrieve values for the second set of inputs
-        school_submit2 = request.POST.get('school_submit2')
-        district_submit2 = request.POST.get('district_submit2')
-        year_submit2 = request.POST.get('year_submit2')
-
-        # Filters for the first set of inputs
-        filters1 = {}
-
-        if school_submit != "Select School":
-            filters1['school'] = school_submit
-        if district_submit != "Select District":
-            filters1['district'] = district_submit
-        if year_submit != "Select Years":
-            filters1['years'] = year_submit
-
-        # Filters for the second set of inputs
-        filters2 = {}
-
-        if school_submit2 != "Select School 2":
-            filters2['school'] = school_submit2
-        if district_submit2 != "Select District 2":
-            filters2['district'] = district_submit2
-        if year_submit2 != "Select Years 2":
-            filters2['years'] = year_submit2
-
-        # Query db for data or set to empty list if filters are empty
-        data1 = enrollment.objects.filter(**filters1) if filters1 else []
-        data2 = enrollment.objects.filter(**filters2) if filters2 else []
-
-        # Combine data1 and data2 using zip_longest
-        combined_enrollment = itertools.zip_longest(data1, data2, fillvalue=None)
-
-        # Create context
-        context = {
-            'combined_enrollment': combined_enrollment,
-            'school_submit': school_submit,
-            'district_submit': district_submit,
-            'year_submit': year_submit,
-            'school_submit2': school_submit2,
-            'district_submit2': district_submit2,
-            'year_submit2': year_submit2,
-        }
+        context = {}
         return render(request, "comparison.html", context)
     else:
         return render(request, "comparison.html", {})
